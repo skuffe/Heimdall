2a07d807e0407056c5e907092dbd4bcb7fc64f9f
Danni Skov Høglund <skuffe@pwnz.dk>
2013-08-20 08:24:30 +0200

    Readme removed - code cleaned

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -15,19 +15,5 @@ namespace FrontEnd.Controllers
 
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
